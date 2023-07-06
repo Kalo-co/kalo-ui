2379cb4cc138529c061593f6d3238a0795a1068c
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-07-05 20:08:16 -0500

    order projects button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,17 @@ import { projects } from "@/data/projects";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <div className='grid gap-4 px-14 py-8 bg-white rounded-md max-h'>
-        <div className="grid gap-4">
+      <div className='grid gap-4 px-14 py-8 bg-white rounded-md max-h shadow-lg'>
+        <div className="flex  justify-between">
           <div className="grid gap-2 lg:max-w-[412px]">
             <h1 className='text-2xl font-bold'>Dashboard de proyectos</h1>
             <p className='text-sm text-[#A4A4A4]'>Explora los proyectos en los que estás trabajando y revisa el estado en el que se encuentran</p>
+          </div>
+
+          <div>
+            <button className="bg-[#F1F1F1] px-5 py-2 text-sm flex gap-4 items-center">
+              Ordenar proyectos <img src="/icons/v.svg"/>
+            </button>
           </div>
         </div>
 
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,7 @@
 type Props = { name: string, progress: string}
 const ProjectCard = (props: Props) => (
     <div className={`relative w-[267.10px] h-[133.64px] cursor-pointer hover:scale-105
-      bg-gradient-to-l from-indigo-400 to-sky-600 rounded-lg shadow-inner`}
+      bg-gradient-to-l from-indigo-400 to-sky-600 rounded-lg shadow-md`}
     >
         <div className="w-full absolute bottom-0 px-8 py-4 text-white">
             <p className="font-semibold text-lg">{props.name}</p>
