d768253e5c65cd4674d212b1254fda8bfdeb235b
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-07-05 21:31:37 -0500

    project status finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,10 @@ export default function Home() {
   )
 
   const Accordeon = (props: { title: string }) => (
-    <div className="border-2 rounded-md px-5 py-4">
+    <div className="border-2 rounded-md px-5 py-4 cursor-pointer">
       <div className="flex justify-between items-center">
         <p className="font-semibold">{props.title}</p>
-        <img src="/icons/chevron.svg"/>
+        <img src="/icons/chevron.svg" />
       </div>
     </div>
   )
@@ -60,7 +60,7 @@ export default function Home() {
         <ProjectsList />
       </div>
 
-      <div className="grid grid-cols-2 gap-4 px-14 py-8 bg-white rounded-md max-h shadow-2xl">
+      <div className="grid grid-cols-2 gap-8 px-14 py-8 bg-white rounded-md max-h shadow-2xl">
         <div className="grid gap-4">
           <div className="grid gap-2">
             <p className="text-xl font-bold">Status del proyecto</p>
@@ -75,6 +75,31 @@ export default function Home() {
             <Accordeon title="Bitácora de avances" />
             <Accordeon title="Entregables" />
             <Accordeon title="Gestión de tiempo" />
+          </div>
+        </div>
+
+        <div className="grid justify-center border rounded-lg shadow-xl">
+          <div className="flex items-center px-10 border-b-2  gap-4">
+            <img className="rounded-full object-cover w-[63px] h-[63px]" src="/images/project-manager.png" />
+            <div>
+              <p className="font-semibold text-sm">Aaron Díaz. Project Manager</p>
+              <p className="text-xs">Nuestro equipo se encuentra trabajando en la codificación de un prototipo hecho en Figma para UPSIDE. El prototipo da una guía a los programadores para hacer funcional un plugin que resuma de manera efectiva la información tomada</p>
+            </div>
+          </div>
+          <div className="px-10 py-4 grid gap">
+            <p className="font-semibold">Objetivos generales:</p>
+            <ul className="list-disc px-8 text-sm">
+              <li>Hacer funcional el sistema de reconocimiento de voz</li>
+              <li>Apoyarse en figma de modo en que la construcción del software siga su linea gráfica</li>
+              <li>Entregar el código funcional para <b>Julio 8</b></li>
+            </ul>
+          </div>
+
+          <div className="px-10 text-sm">
+            <p>El 65% de este proyecto está listo</p>
+            <div className="mt-1 w-full border h-[9px] rounded-lg overflow-hidden border-blue-600"> 
+              <div className="w-4/6 bg-blue-500 h-full"></div>
+            </div>
           </div>
         </div>
       </div>
